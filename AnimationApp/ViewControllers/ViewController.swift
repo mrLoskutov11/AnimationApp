dfd42695366b98f03303e465f0881aa90f3e2f0d
Anton Loskutov <a424056193@me.com>
2022-11-02 22:51:02 +0300

    Create outlets

--- AnimationApp/ViewControllers/ViewController.swift
+++ AnimationApp/ViewControllers/ViewController.swift
@@ -8,7 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var coreAnimationView: UIView!
+    
+    @IBOutlet var presetLable: UILabel!
+    @IBOutlet var curveLabel: UILabel!
+    @IBOutlet var forceLabel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var delayLable: UILabel!
+    
+    @IBOutlet var startAnimationButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
