021a57cd9bcc2ed847cf703c70f7d712fe5bf0c5
Anton Loskutov <a424056193@me.com>
2022-11-02 21:58:35 +0300

    Create Folders

---
+++ Animation.swift
@@ -0,0 +1,8 @@
+//
+//  Animation.swift
+//  AnimationApp
+//
+//  Created by Антон Лоскутов on 02.11.2022.
+//
+
+import Foundation
---
+++ DataStore.swift
@@ -0,0 +1,8 @@
+//
+//  DataStore.swift
+//  AnimationApp
+//
+//  Created by Антон Лоскутов on 02.11.2022.
+//
+
+import Foundation
