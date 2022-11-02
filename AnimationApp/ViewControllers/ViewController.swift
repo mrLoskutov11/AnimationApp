70f832af62f58282e3200a7c37d249254e58db8a
Anton Loskutov <a424056193@me.com>
2022-11-03 00:20:06 +0300

    Done!

--- AnimationApp/ViewControllers/ViewController.swift
+++ AnimationApp/ViewControllers/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import SpringAnimation
 
 class ViewController: UIViewController {
-    @IBOutlet var coreAnimationView: UIView!
+    @IBOutlet var coreAnimationView: SpringView!
     
     @IBOutlet var presetLable: UILabel!
     @IBOutlet var curveLabel: UILabel!
@@ -20,10 +20,38 @@ class ViewController: UIViewController {
     @IBOutlet var startAnimationButton: UIButton!
     
     private var animation = Animation.getAnimation()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupLabels()
     }
 
-
+    
+    @IBAction func runAnimationButtonDidTapedd() {
+            setupAnimation()
+            setupLabels()
+            coreAnimationView.animate()
+            
+            animation = Animation.getAnimation()
+            startAnimationButton.setTitle("Run \(animation.preset)", for: .normal)
+    }
+    
+    private func setupAnimation() {
+        coreAnimationView.animation = animation.preset
+        coreAnimationView.curve = animation.curve
+        coreAnimationView.force = animation.force
+        coreAnimationView.duration = animation.duration
+        coreAnimationView.delay = animation.delay
+    }
+    private func setupLabels() {
+        presetLable.text = ("Preset: \(animation.preset)")
+        curveLabel.text = ("Curve: \(animation.curve)")
+        forceLabel.text = ("Forse: \(string(from: animation.force))")
+        durationLabel.text = ("Duration: \(string(from: animation.duration))")
+        delayLable.text = ("Delay: \(string(from: animation.delay))")
+    }
+    private func string(from double: Double) -> String {
+        String(format: "%.2f", double)
+    }
 }
 
